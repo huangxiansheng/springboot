ed7f460eecf8d8968a59c73b62ffed3a6ef791e5
huangxiansheng <hsq54321@126.com>
2018-05-30 16:30:17 +0800

    初版
    app

---
+++ src/main/java/com/hs/springboot/Application.java
@@ -0,0 +1,18 @@
+package com.hs.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *@描述 
+ *@日期 2018年5月15日上午9:19:50
+ *@作者 vpadmin
+ */
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/hs/springboot/controller/AController.java
@@ -0,0 +1,41 @@
+package com.hs.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *@描述 
+ *@日期 2018年5月15日上午9:22:37
+ *@作者 vpadmin
+ */
+//@RestController = @Controller + @ResponseBody
+@Controller
+public class AController {
+
+	@RequestMapping("/a")
+	@ResponseBody
+	public String index() {
+		return "index page";
+	}
+	
+	@RequestMapping("/th1")
+	public String th1(Model model) {
+		model.addAttribute("name", "Dear");
+		return "a";
+	}
+	@RequestMapping("/th2")
+	public String th2() {
+		
+		return "b";
+	}
+	
+	@RequestMapping("/9xx")
+	public String xx() {
+		if(true) {
+			throw new RuntimeException("daslflskdfj");
+		}
+		return "b";
+	}
+}
